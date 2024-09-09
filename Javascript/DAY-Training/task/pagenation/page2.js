a01095b985a2a56176bfe3e32a3126000be8bb00
kishore-85 <kishore190120@gmail.com>
2024-09-09 12:26:57 +0530

    dynamic pagenation update2

--- Javascript/DAY-Training/task/pagenation/page2.js
+++ Javascript/DAY-Training/task/pagenation/page2.js
@@ -28,20 +28,31 @@ let page= ()=>{
 function createpage(e){  
    console.log('clicked',e)
       let a = parseInt(e);     
-      let get_arrid; 
+      let get_arrid;     
      
-     
-    //   let find_index;
+   
       for(let i in fltr_data){
-           if(i==a){                                 
+           if(i==a){                                             //finding starting id;
             get_arrid =  fltr_data[i].id           
            }
       }       
-      let b = get_arrid+5;     
-      for(let i=get_arrid-1;i<b-1;i++){          
-        let p =  document.createElement('p');
-        p.innerText=`${data[i].id}) ${data[i].title} `
-        div1.appendChild(p)   }
+      let b = get_arrid+5;    
+
+    //   for(let i=get_arrid-1;i<b-1;i++){          
+    //     let p =  document.createElement('p');                  //traditional for loop method
+    //     p.innerText=`${data[i].id}) ${data[i].title} `
+    //     div1.appendChild(p)  
+    //  }
+
+    let filtered_data = data.filter((val)=>{                      //filter method
+        return val = val.id>= get_arrid && val.id <b
+    })
+   
+    filtered_data.forEach((x) => {
+        let p =  document.createElement('p');                  
+            p.innerText=`${x.id}) ${x.title} `
+            div1.appendChild(p) 
+    });
       
     
 }
