0161ddf14dfff1dc4eeec1c64e23264097963561
kishore-85 <kishore190120@gmail.com>
2024-09-09 11:11:31 +0530

    dynamic pagenation update

--- Javascript/DAY-Training/task/pagenation/page2.js
+++ Javascript/DAY-Training/task/pagenation/page2.js
@@ -1,66 +1,56 @@
-let data = []
-let div =document.getElementById('div')
+let data = [];
+let fltr_data = [];
+let div1 =document.getElementById('div')
 let pages;
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(res=>res.json())
-.then(resp=>{
-    console.log(resp,'resp')    
-    for(let i of resp){
-        data.push(i)
-    }
+.then(resp=>{     
+    data = JSON.parse(JSON.stringify(resp))    
 })
 .catch(err=>{
-    console.log(err)
-   
+    console.log(err)   
 })
-
-
-
-
-setTimeout(() => {   
-    let len= []
-    data.forEach(x => {
-       len.push(x.id)       
-    });
-   length = len.length;  
-   pages = length/10;
+setTimeout(() => {     
+   length = fltr_data.length;   
    page(); 
  
-}, 2000);
+}, 3000);
 
 
-let page= ()=>{
-    for(i=1;i<=pages;i++){
-        let a = document.createElement('a');
-        a.setAttribute('id','link')
-        a.setAttribute('href',`#`)
-        a.innerText= i;
-        div.appendChild(a)
+let page= ()=>{   
+    for(i=0;i<length;i++){       
+        let a =  document.createElement('a')
+        a.innerHTML = `<a id="link" href="#" onclick="createpage(${i})"> ${i} </a>`
+        div1.appendChild(a);     
+     }
     }
-}
-
-setTimeout(() => {
-    document.querySelectorAll('a').forEach(e => {
-        e.addEventListener('click',(e)=>{        
-          console.log('clicked')
-          createpage(e.target.innerText)
-        })
-  });
-  
-}, 2500);
-
-let items;
-let ftr_data = [];
-function createpage(e){
-    //   console.log(e,'e')
-    //   for(i=0;i<data.length;i++){
-    //      for(j=i;j<j+5;j+5){
-    //         console.log(data[j],'data of j')
-    //      }
-    //   }
+   
+function createpage(e){  
+   console.log('clicked',e)
+      let a = parseInt(e);     
+      let get_arrid; 
+     
+     
+    //   let find_index;
+      for(let i in fltr_data){
+           if(i==a){                                 
+            get_arrid =  fltr_data[i].id           
+           }
+      }       
+      let b = get_arrid+5;     
+      for(let i=get_arrid-1;i<b-1;i++){          
+        let p =  document.createElement('p');
+        p.innerText=`${data[i].id}) ${data[i].title} `
+        div1.appendChild(p)   }
+      
     
 }
 
+
+
 setTimeout(() => {
-    console.log(d,'d')
-}, 5000);
+    for(let i=0;i<data.length;i+=5){
+           fltr_data.push(data[i]) 
+    }
+    console.log('pushed data',fltr_data)
+}, 2000);
